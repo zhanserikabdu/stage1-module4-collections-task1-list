c2832eb46ec2b2075e6783aadb3e64555af015f2
Nadzeya_Chybisava <nadezhdalobanok@gmail.com>
2022-08-02 14:54:08 +0300

    change pom and tests to parametrized tests

--- src/test/java/com/epam/mjc/collections/list/LinkedListCreatorTest.java
+++ src/test/java/com/epam/mjc/collections/list/LinkedListCreatorTest.java
@@ -1,50 +1,44 @@
 package com.epam.mjc.collections.list;
 
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LinkedListCreatorTest {
+    public static Stream<Arguments> testCases() {
+        return Stream.of(
+                Arguments.of("EmptySourceList",
+                        List.of(),
+                        new LinkedList<>()),
 
-    @Test
-    public void createLinkedListFromEmptyList() {
-        LinkedListCreator linkedListCreator = new LinkedListCreator();
-        LinkedList<Integer> expectedLinkedList = new LinkedList<>();
+                Arguments.of("FromListOfOddNumbers",
+                        List.of(47, 3, 25, 13, 7),
+                        new LinkedList<>(List.of(7, 13, 25, 3, 47))),
 
-        LinkedList<Integer> actualLinkedList = linkedListCreator.createLinkedList(new ArrayList<>());
-        assertEquals(expectedLinkedList, actualLinkedList);
+                Arguments.of("FromListOfEvenNumbers",
+                        List.of(12, 44, 80, 2, 52),
+                        new LinkedList<>(List.of(12, 44, 80, 2, 52))),
+
+                Arguments.of("FromListOfOddAndEvenNumbers",
+                        List.of(47, 3, 12, 25, 44, 80, 13, 2, 7, 52),
+                        new LinkedList<>(List.of(7, 13, 25, 3, 47, 12, 44, 80, 2, 52)))
+        );
     }
 
-    @Test
-    public void createLinkedListFromListOfOddNumbers() {
-        LinkedListCreator linkedListCreator = new LinkedListCreator();
-        LinkedList<Integer> expectedLinkedList = new LinkedList<>(List.of(7, 13, 25, 3, 47));
+    @ParameterizedTest(name = "createLinkedList_{0}_Test")
+    @MethodSource(value = "testCases")
+    void createLinkedListTest(String name,
+                              List<Integer> sourceList,
+                              LinkedList<Integer> expectedLinkedList) {
 
-        LinkedList<Integer> actualLinkedList = linkedListCreator.createLinkedList(List.of(47, 3, 25, 13, 7));
+        LinkedListCreator linkedListCreator = new LinkedListCreator();
+        LinkedList<Integer> actualLinkedList = linkedListCreator.createLinkedList(sourceList);
         assertEquals(expectedLinkedList, actualLinkedList);
     }
-
-
-    @Test
-    public void createLinkedListFromListOfEvenNumbers() {
-        LinkedListCreator linkedListCreator = new LinkedListCreator();
-        LinkedList<Integer> expectedLinkedList = new LinkedList<>(List.of(12, 44, 80, 2, 52));
-
-        LinkedList<Integer> actualLinkedList = linkedListCreator.createLinkedList(List.of(12, 44, 80, 2, 52));
-        assertEquals(expectedLinkedList, actualLinkedList);
-    }
-
-    @Test
-    public void createLinkedListFromListOfOddAndEvenNumbers() {
-        LinkedListCreator linkedListCreator = new LinkedListCreator();
-        LinkedList<Integer> expectedLinkedList = new LinkedList<>(List.of(7, 13, 25, 3, 47, 12, 44, 80, 2, 52));
-
-        LinkedList<Integer> actualLinkedList = linkedListCreator.createLinkedList(List.of(47, 3, 12, 25, 44, 80, 13, 2, 7, 52));
-        assertEquals(expectedLinkedList, actualLinkedList);
-    }
-
 }
--- src/test/java/com/epam/mjc/collections/list/ListSorterTest.java
+++ src/test/java/com/epam/mjc/collections/list/ListSorterTest.java
@@ -1,30 +1,35 @@
 package com.epam.mjc.collections.list;
 
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ListSorterTest {
+    public static Stream<Arguments> testCases() {
+        return Stream.of(
+                Arguments.of("EmptyList",
+                        new ArrayList<>(),
+                        List.of()),
 
-    @Test
-    public void sortEmptyList() {
-        ListSorter listSorter = new ListSorter();
-        List<String> sourceList = new ArrayList<>();
-        List<String> expectedList = List.of();
-
-        listSorter.sort(sourceList);
-        assertEquals(expectedList, sourceList);
+                Arguments.of("NonEmptyList",
+                        new ArrayList<>(List.of("7", "10", "6", "-7", "-2", "0", "-2", "-23")),
+                        List.of("0", "-2", "-2", "6", "-7", "7", "10", "-23"))
+        );
     }
 
-    @Test
-    public void sortNonEmptyList() {
-        ListSorter listSorter = new ListSorter();
-        List<String> sourceList = new ArrayList<>(List.of("7", "10", "6", "-7", "-2", "0", "-2", "-23"));
-        List<String> expectedList = List.of("0", "-2", "-2", "6", "-7", "7", "10", "-23");
+    @ParameterizedTest(name = "sort_{0}_Test")
+    @MethodSource(value = "testCases")
+    void sortTest(String name,
+                  List<String> sourceList,
+                  List<String> expectedList) {
 
+        ListSorter listSorter = new ListSorter();
         listSorter.sort(sourceList);
         assertEquals(expectedList, sourceList);
     }
